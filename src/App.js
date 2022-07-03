0066973959d776c37ab38445927d32d55631593a
Pablo Pardo <pablopardoalcocer@gmail.com>
2022-07-03 05:13:49 -0400

    Init project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/admin" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
